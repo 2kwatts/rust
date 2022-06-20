87ed79c286541a564f34a50a3711a3c1af831ad4
2kwatts <2k@fake.dumb>
2022-06-19 21:28:19 -0400

    main.rs generates random numbers now ig

--- rustuff/src/main.rs
+++ rustuff/src/main.rs
@@ -1,24 +1,11 @@
-use std::any::type_name; // for type_of
-
-fn type_of<T>(_: T) -> &'static str {
-    type_name::<T>()
-}
+use rand::Rng; // 0.8.0
 
 fn main() {
-    /* 'mut' makes the variable mutable, meaning that it's value
-        will be (or can be) changed later in the program.
-        Variables are immutable by default, so you'll probably
-        want to be adding it most of the time.
-    */
-    let mut x = 5;
-    println!("Hello, World!");
-    get_value(x);
-    println!("Changing 'x' to 500 now.");
-    x = 500; // let is not needed for adding new values to mutable vars.
-    get_value(x);
-}
+    let mut n = 1;
 
-fn get_value(x: i32) {
-    // Get value of variable and print it (+ var type)
-    println!("Variable x (type: {}) is equal to: {}", type_of(x), x);
+    while n < 100 {
+    let num = rand::thread_rng().gen_range(0..1000001);
+    println!("{}", num);
+    n += 1; 
+    }
 }
